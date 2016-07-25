d5604f701b9ada2b9891326fdfbfb2441a56e036
witwiki <Vikram.Udyawer@community.isunet.edu>
2016-07-25 18:52:05 +1200

    Completed lesson 2 of the miwok app

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -1,13 +1,49 @@
 package com.example.android.miwok;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_colors);
+        setContentView(R.layout.activity_wordslist);
+
+        //  Create a list of words based on Custom Class Word.java
+        ArrayList<Word> words = new ArrayList<Word>();
+
+        //  Instantiate and adding default and miwok words
+        words.add(new Word("red", "weṭeṭṭi"));
+        words.add(new Word("green", "chokokki"));
+        words.add(new Word("brown", "ṭakaakki"));
+        words.add(new Word("grey", "ṭopoppi"));
+        words.add(new Word("black", "kululli"));
+        words.add(new Word("white", "kelelli"));
+        words.add(new Word("dusty yellow", "ṭopiisә"));
+        words.add(new Word("mustard yellow", "chiwiiṭә"));
+
+        /**
+         *  Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
+         adapter knows how to create list items for each item in the list.
+         */
+        WordAdapter adapter = new WordAdapter(this, words);
+
+        /**
+         Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+         There should be a {@link ListView} with the view ID called list, which is declared in the
+         activity_wordslist.xmlml layout file.
+         */
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        /**
+         *  Make the {@link ListView} use the {@link WordAdapter} we created above, so that the
+         *  {@link ListView} will display list items for each {@link Word} in the list.
+         */
+        listView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,11 +3,7 @@ package com.example.android.miwok;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -16,7 +12,7 @@ public class NumbersActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers);
+        setContentView(R.layout.activity_wordslist);
 
         //  Create a list of words based on Custom Class Word.java
         ArrayList<Word> words = new ArrayList<Word>();
@@ -42,7 +38,7 @@ public class NumbersActivity extends AppCompatActivity {
         /**
          Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
          There should be a {@link ListView} with the view ID called list, which is declared in the
-         activity_numbers.xml layout file.
+         activity_wordslist.xmlml layout file.
         */
         ListView listView = (ListView) findViewById(R.id.list);
 
